c174cd7f4bd8f06dda9147c0ba8c35cf9a490fa4
[dyad] <tonestrife@hotmail.com>
2026-05-17 21:02:11 +0200

    [dyad] Eliminada cabecera duplicada en App.tsx - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import { SettingsPage } from './pages/SettingsPage';
 import { UsersPage } from './pages/UsersPage';
 import { FreezerDetailPage } from './pages/FreezerDetailPage';
 import { BoxDetailPage } from './pages/BoxDetailPage';
-import { AppLayout } from './components/AppLayout';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
@@ -21,21 +20,19 @@ function App() {
         path="/*"
         element={
           <ProtectedRoute>
-            <AppLayout>
-              <Routes>
-                <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/freezers" element={<FreezersPage />} />
-                <Route path="/freezers/:id" element={<FreezerDetailPage />} />
-                <Route path="/freezers/:freezerId/box/:boxId" element={<BoxDetailPage />} />
-                <Route path="/boxes" element={<BoxesPage />} />
-                <Route path="/samples" element={<SamplesPage />} />
-                <Route path="/search" element={<SearchPage />} />
-                <Route path="/reports" element={<ReportsPage />} />
-                <Route path="/settings" element={<SettingsPage />} />
-                <Route path="/users" element={<UsersPage />} />
-                <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              </Routes>
-            </AppLayout>
+            <Routes>
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/freezers" element={<FreezersPage />} />
+              <Route path="/freezers/:id" element={<FreezerDetailPage />} />
+              <Route path="/freezers/:freezerId/box/:boxId" element={<BoxDetailPage />} />
+              <Route path="/boxes" element={<BoxesPage />} />
+              <Route path="/samples" element={<SamplesPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/reports" element={<ReportsPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+              <Route path="/users" element={<UsersPage />} />
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            </Routes>
           </ProtectedRoute>
         }
       />
