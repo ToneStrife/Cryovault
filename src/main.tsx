019d7543e2848c099f9dc53dd45a89a4d4e0f5fa
[dyad] <tonestrife@hotmail.com>
2026-05-17 19:49:56 +0200

    [dyad] He envuelto la aplicación en QueryClientProvider para solucionar el error de contexto de React Query. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
-      <BrowserRouter basename="/Cryovault/">
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter basename="/Cryovault/">
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </React.StrictMode>
   );
 } else {
