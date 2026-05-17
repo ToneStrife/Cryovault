4fa6297f988da0f29c4ed1eea5987f54511aa897
[dyad] <tonestrife@hotmail.com>
2026-05-17 20:06:12 +0200

    [dyad] Registrando todas las rutas necesarias para la navegación de la aplicación. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { DashboardPage } from './pages/DashboardPage';
 import { BoxesPage } from './pages/BoxesPage';
+import { FreezersPage } from './pages/FreezersPage';
+import { SamplesPage } from './pages/SamplesPage';
+import { SearchPage } from './pages/SearchPage';
+import { ReportsPage } from './pages/ReportsPage';
+import { SettingsPage } from './pages/SettingsPage';
+import { UsersPage } from './pages/UsersPage';
+import { FreezerDetailPage } from './pages/FreezerDetailPage';
+import { BoxDetailPage } from './pages/BoxDetailPage';
 import { AppLayout } from './components/AppLayout';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
@@ -16,7 +24,15 @@ function App() {
             <AppLayout>
               <Routes>
                 <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/freezers" element={<FreezersPage />} />
+                <Route path="/freezers/:id" element={<FreezerDetailPage />} />
+                <Route path="/freezers/:freezerId/box/:boxId" element={<BoxDetailPage />} />
                 <Route path="/boxes" element={<BoxesPage />} />
+                <Route path="/samples" element={<SamplesPage />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/reports" element={<ReportsPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/users" element={<UsersPage />} />
                 <Route path="/" element={<Navigate to="/dashboard" replace />} />
               </Routes>
             </AppLayout>
