fa5549b650cafa32250a5fe75810184000dcb2de
ToneStrife <tonestrife@hotmail.com>
2026-05-19 01:49:07 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
@@ -14,12 +14,11 @@ if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        {/* Cambia esto al nombre de tu repo */}
-        <HashRouter basename="/Cryovault/">
+         <BrowserRouter basename="/Cryovault/">
           <AuthProvider>
             <App />
           </AuthProvider>
-        </HashRouter>
+        </BrowserRouter>
       </QueryClientProvider>
     </React.StrictMode>
   );
